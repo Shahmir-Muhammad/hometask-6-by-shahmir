df73e3b7b76a928123ad85a35d9ac3cfbbd74e83
Shahmir-Muhammad <147588981+Shahmir-Muhammad@users.noreply.github.com>
2023-11-28 12:14:01 -0800

    Add files via upload

---
+++ lab 6.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+int main(){
+	
+	
+                            	// TASK 1
+	
+	
+/*int sum=0;
+for(int i=0;i<50;i++){
+if(i%2!=0){
+	sum+=i;
+continue;}
+cout<<sum<<endl;
+
+}
+return 0;
+
+}*/
+
+
+                                 // TASK 2
+
+ /*int n=0;
+cout<<"enter n :"<<endl;
+cin>>n;
+for(int i=1; i<=n; i++){
+	for(int j=1;j<=i;j++){
+		cout<<' '<<endl;
+		cout<<i<<endl;
+	}
+	
+}return 0;
+
+                                 // TASK 3
+ int n=0;
+cout<<"enter n :"<<endl;
+cin>>n;
+for(int i=1; i<=n; i++){
+	for(int j=1;j<=i;j++){
+		cout<<' '<<endl;
+		cout<<j<<endl;
+	}
+	
+}
+return 0;  }
